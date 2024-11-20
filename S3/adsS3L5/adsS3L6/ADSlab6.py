9046ff63047345480e8f44a9ca212d898d97b56c
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-11-20 13:58:45 +0200

    Add files via upload

---
+++ S3/adsS3L5/adsS3L6/ADSlab6.py
@@ -0,0 +1,19 @@
+import numpy as np
+import time
+
+def sort(mass):
+    for i in range(len(mass)-1):
+        i_min = i
+        for j in range(i+1, len(mass)):
+            if mass[j] < mass[i_min]:
+                mass[i], mass[j] = mass[j], mass[i]
+
+    return mass
+
+
+mass = np.random.randint(1, 100000, size=1000)
+start_time = time.time()
+sort(mass)
+end_time = time.time()
+print(end_time - start_time)
+print(mass)
