cf1dd1fc8fd94a9feba91c2aacca1961d2109b36
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-11-30 22:23:17 +0200

    Update ADSlab13.py

--- S3/adsS3L13/ADSlab13.py
+++ S3/adsS3L13/ADSlab13.py
@@ -1,45 +1,26 @@
 class HashTable:
     def __init__(self, capacity=100):
         self.capacity = capacity
-        self.table = [[] for _ in range(capacity)]  # Инициализируем таблицу списками
+        self.table = [[] for _ in range(capacity)]
 
     def _hash(self, key):
-        """Хеш-функция для получения индекса."""
         return hash(key) % self.capacity
 
     def insert(self, key, value):
-        """Вставка нового ключа-значения в хеш-таблицу."""
         index = self._hash(key)
-        # Проверяем, существует ли ключ, и обновляем значение, если он существует
+
         for item in self.table[index]:
             if item[0] == key:
                 item[1] = value
                 return
-        # Если ключ не найден, добавляем новую пару
         self.table[index].append([key, value])
-
-    def search(self, key):
-        """Поиск значения по ключу."""
-        index = self._hash(key)
-        for item in self.table[index]:
-            if item[0] == key:
-                return item[1]  # Возвращаем значение
-        return None  # Если ключ не найден
-
-    def delete(self, key):
-        """Удаление ключа из хеш-таблицы."""
-        index = self._hash(key)
-        for i, item in enumerate(self.table[index]):
-            if item[0] == key:
-                del self.table[index][i]  # Удаляем элемент из списка
-                return
 
     def save(self, path):
         """Сохранение хеш-таблицы в файл."""
         with open(path, 'w') as f:
             for bucket in self.table:
                 for item in bucket:
-                    f.write(f"{item[0]},{item[1]}\n")  # Сохраняем ключ и значение
+                    f.write(f"{item[0]},{item[1]}\n")
 
 ht = HashTable()
 f = open('input.txt', 'r', encoding="utf-8")
